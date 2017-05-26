3c3fabb7dba16e128f8c3c580046e0626156e332
maxai <maxai@futunn.com>
2017-05-26 15:24:58 +0800

    add typing function

--- index.js
+++ index.js
@@ -9,25 +9,31 @@ const io = require('socket.io')(server);
 
 app.use(serve(path.join(__dirname, '/')));
 
-let uid = 1;
+let uid = 1; // 用户 id
 
+// 用户连接事件
 io.on('connection', (socket) => {
   socket.emit('set name', `user${uid}`);
-  socket.request.headers.cookie += `; id=${uid}`;
+  socket.request.headers.cookie += `; id=${uid}`; // 设置 cookie 保存用户 id 用于记录用户离线
 
   const connectedMsg = `user${uid} connected`;
   console.log(connectedMsg);
-  io.emit('chat message', connectedMsg);
+  io.emit('chat message', connectedMsg);  // 新用户用户连接消息
   
   uid ++;
 
+  // 用户断开连接
   socket.on('disconnect', () => {
     const disMsg = `user${cookie.parse(socket.request.headers.cookie).id} disconnect`;
     console.log(disMsg);
-    io.emit('chat message', disMsg);
+    io.emit('chat message', disMsg);  // 用户离线消息
   });
 
+  // 接收用户消息并转发给除自己之外的所有人
   socket.on('chat message', (msg) =>  socket.broadcast.emit('chat message', msg));
+
+  // 显示"正在输入"功能
+  socket.on('typing', () => socket.broadcast.emit('typing', cookie.parse(socket.request.headers.cookie).id));
 });
 
 server.listen(3000);
