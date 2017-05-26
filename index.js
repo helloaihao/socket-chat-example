8e9f809acc07f0bb3d408cd162ca74d25a7e94e9
maxai <maxai@futunn.com>
2017-05-26 11:18:37 +0800

    add connect info and disconnect info

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const serve = require('koa-static');
 const path = require('path');
+const cookie = require('cookie');
 
 const app = new Koa();
 const server = require('http').createServer(app.callback());
@@ -11,10 +12,20 @@ app.use(serve(path.join(__dirname, '/')));
 let uid = 1;
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('set name', `user${uid ++}`);
+  socket.emit('set name', `user${uid}`);
+  socket.request.headers.cookie += `; id=${uid}`;
+  
+  const connectedMsg = `user${uid} connected`;
+  console.log(connectedMsg);
+  io.emit('chat message', connectedMsg);
+  
+  uid ++;
 
-  socket.on('disconnect', () => console.log('user disconnected'));
+  socket.on('disconnect', () => {
+    const disMsg = `user${cookie.parse(socket.request.headers.cookie).id} disconnect`;
+    console.log(disMsg);
+    io.emit('chat message', disMsg);
+  });
 
   socket.on('chat message', (msg) => io.emit('chat message', msg));
 });
